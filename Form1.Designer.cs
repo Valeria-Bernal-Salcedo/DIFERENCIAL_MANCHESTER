f0363446e6359de895e14c90243f9641a704cd4b
Valeria Bernal Salcedo <vabersal05@gmail.com>
2025-05-14 09:26:12 -0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DIFERENCIAL_MANCHESTER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.signal = new System.Windows.Forms.TextBox();
+            this.GRAFICAR = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.difManchester = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.difManchester)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SEÑAL A GRAFICAR:";
+            // 
+            // signal
+            // 
+            this.signal.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signal.Location = new System.Drawing.Point(474, 100);
+            this.signal.Name = "signal";
+            this.signal.Size = new System.Drawing.Size(290, 35);
+            this.signal.TabIndex = 2;
+            this.signal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // GRAFICAR
+            // 
+            this.GRAFICAR.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GRAFICAR.Location = new System.Drawing.Point(835, 100);
+            this.GRAFICAR.Name = "GRAFICAR";
+            this.GRAFICAR.Size = new System.Drawing.Size(277, 58);
+            this.GRAFICAR.TabIndex = 3;
+            this.GRAFICAR.Text = "GRAFICAR";
+            this.GRAFICAR.UseVisualStyleBackColor = true;
+            this.GRAFICAR.Click += new System.EventHandler(this.GRAFICAR_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(442, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(365, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "SEÑAL DIFERENCIAL MANCHESTER";
+            // 
+            // difManchester
+            // 
+            this.difManchester.Location = new System.Drawing.Point(58, 203);
+            this.difManchester.Name = "difManchester";
+            this.difManchester.Size = new System.Drawing.Size(1137, 253);
+            this.difManchester.TabIndex = 8;
+            this.difManchester.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1248, 723);
+            this.Controls.Add(this.difManchester);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.GRAFICAR);
+            this.Controls.Add(this.signal);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.difManchester)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox signal;
+        private System.Windows.Forms.Button GRAFICAR;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox difManchester;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DIFERENCIAL_MANCHESTER
+{
+    public partial class Form1 : Form
+    {
+        string bits;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GRAFICAR_Click(object sender, EventArgs e)
+        {
+
+            bits = signal.Text;
+
+            if (!bits.All(c => c == '0' || c == '1'))
+            {
+                MessageBox.Show("Solo se permiten 0 y 1");
+                return;
+            }
+
+            difManchester.Paint += Dibujar;
+            difManchester.Refresh();
+
+        }
+
+        private void Dibujar(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; // 'Lienzo' para dibujar
+            Pen pen = new Pen(Color.Blue, 2); // 'Lapiz' azul para la señal
+            Pen gridPen = new Pen(Color.LightGray, 1); // 'Lapiz' gris para la cuadricula
+            Font font = new Font("Arial", 10); // Fuente
+
+            // Coordenadas base
+            int xInicio = 50; // Donde empieza el dibujo en X
+            int yCero = 150; // Coordenada vertical para el nivel 0
+            int yUno = 50; // Coordenada vertical para el nivel 1
+            int ancho = 50; // Ancho que ocupa 1 bit
+            int mitad = ancho / 2;
+
+            // Etiquetas '0' y '1' en el eje Y
+            g.DrawString("1", font, Brushes.Black, 20, yUno - 10);
+            g.DrawString("0", font, Brushes.Black, 20, yCero - 10);
+
+            // Dibujar líneas verticales de la cuadrícula (1 por cada bit)
+            for (int i = 0; i <= bits.Length; i++)
+            {
+                int x1 = xInicio + i * ancho;
+                g.DrawLine(gridPen, x1, yUno, x1, yCero);
+            }
+
+            // Dibujar líneas horizontales (nivel 1 y 0)
+            g.DrawLine(gridPen, xInicio, yUno, xInicio + bits.Length * ancho, yUno);
+            g.DrawLine(gridPen, xInicio, yCero, xInicio + bits.Length * ancho, yCero);
+
+            // Dibujar los bits
+            int yActual = bits[0]=='1' ? yUno : yCero; // Nivel inicial
+            int x = xInicio;
+
+            for (int i = 0; i < bits.Length; i++)
+            {
+                char bit = bits[i];
+
+                // Dibujar el numero del bit
+                g.DrawString(bits[i].ToString(), font, Brushes.Black, x + 15, yCero + 10);
+
+                bool transicionInicio = (bit == '0');
+
+                /// Parte izquierda (inicio del bit)
+                if (transicionInicio)
+                {
+                    int nuevaY = (yActual == yUno) ? yCero : yUno;
+                    g.DrawLine(pen, x, yActual, x, nuevaY); // transición al inicio
+                    yActual = nuevaY;
+                }
+
+                // Línea horizontal mitad izquierda
+                g.DrawLine(pen, x, yActual, x + mitad, yActual);
+
+                int midY = (yActual == yUno) ? yCero : yUno;
+                g.DrawLine(pen, x + mitad, yActual, x + mitad, midY);
+                yActual = midY;
+
+                // Dibujar línea mitad derecha
+                g.DrawLine(pen, x + mitad, yActual, x + ancho, yActual);
+
+                // Preparar siguiente bit
+                x += ancho;
+            }
+
+            // Dibujar flecha final
+            g.DrawLine(pen, x, yActual, x + 10, yActual);
+            g.DrawLine(pen, x + 5, yActual - 5, x + 10, yActual);
+            g.DrawLine(pen, x + 5, yActual + 5, x + 10, yActual);
+        }
+    }
+}
